56c04f910da3a8e100f215796a2f4ff723bac905
Xiang <zhangxiang1@jd.com>
2016-04-19 13:41:43 +0800

    上传存储和请求M3u8

--- mydota/app.js
+++ mydota/app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var video = require('./routes/video');
+var upload = require('./routes/upload');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/video',video);
+app.use('/upload',upload);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- mydota/routes/video.js
+++ mydota/routes/video.js
@@ -2,50 +2,39 @@ var express = require('express');
 var router = express.Router();
 var superagent = require('superagent');
 
-var VideoM3u8 = require('../database/videoM3u8');
-var cheerio = require('cheerio');
+var VideoM3u8 = require('../model/videoM3u8');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-	  var id = req.query.id;
-	  var type = req.query.type;
-	  if (!type) {type = 'mp4'};
+var id = req.query.id;
 if (id) {
 	VideoM3u8.findOne({id:id},function(err,doc){
 		if (err) {
-			console.log(err);
+			res.render('error',{
+				message:err.message,
+				error:err
+			});
+			return;
 		}
 		if (!doc) {
-			superagent.get('http://v.youku.com/v_show/id_'+id+'.html')
-			.set('User-Agent','Mozilla/5.0 (iPhone; CPU iPhone OS 9_3 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13E230 Safari/601.1')
-			.end(function(err,sres){
-				// var $ = cheerio.load(sres.text);
-				// var src = $.BuildVideoInfo.m3u8src(type);
-				// console.log('src == ' + src);
-				// if (src) {
-				// 	req.send(src);
-				// 	return;
-				// }
-				res.send(sres.text);
-			});
+			res.send({'code':0,data:{}});
 		}else{
-			res.send(doc);
+			var result = {'id':doc.id,m3u8:doc.m3u8};
+			res.send({'code':0,data:result});
 		}
 	});
-	return;
-}
-
-
+}else{
 	superagent.get('https://api.youku.com/quality/video/by/category.json?client_id=e2306ead120d2e34&cate=10&count=10')
-	.end(function(err,sres){
-		if (err) {
-			return next(err);
-		}
-	res.send(sres.text);
+			  .end(function(err,sres){
+		  		if (err) {
+					return next(err);
+				}
+				res.send(sres.text);
   	//res.render('video', { title: 'DotA视频' });
 
 	});
+}
 
 });
 
