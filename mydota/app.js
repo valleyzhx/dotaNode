1b5afcdc88c267e934316ac228bce1c55ef84092
Xiang <zhangxiang1@jd.com>
2016-07-06 14:28:56 +0800

    mydota 路径

--- mydota/app.js
+++ mydota/app.js
@@ -26,17 +26,17 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
-app.use('/video',video);
-app.use('/upload',upload);
-app.use('/appConfigure',appConfigure);
+app.use('/mydota/users', users);
+app.use('/mydota/video',video);
+app.use('/mydota/upload',upload);
+app.use('/mydota/appConfigure',appConfigure);
 
 // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 // error handlers
 
